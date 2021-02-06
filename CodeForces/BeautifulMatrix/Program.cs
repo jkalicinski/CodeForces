fc12307b5c5076996c4a9ea0e6b22ae4011d0efd
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-06 14:06:04 +0100

    263A - Beautiful Matrix

---
+++ CodeForces/BeautifulMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BeautifulMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int curPosY;
+            int pos1Y = 0;
+            int pos1X = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (pos1X > 0)
+                    break;
+
+                string line = Console.ReadLine();
+                curPosY = 0;
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == '0' || line[j] == '1')
+                    {
+                        if (line[j] == '1')
+                        {
+                            pos1Y = curPosY;
+                            pos1X = i;
+                            break;
+                        }
+
+                        curPosY++;
+                    }
+                }
+            }
+
+            int diffx = 2 - pos1X;
+            int diffy = 2 - pos1Y;
+
+            if (diffx < 0)
+                diffx = diffx * (-1);
+
+            if (diffy < 0)
+                diffy = diffy * (-1);
+
+            Console.WriteLine(diffx + diffy);
+        }
+    }
+}
