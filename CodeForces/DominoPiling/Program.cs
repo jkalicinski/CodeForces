151e32939b078f0f07f1ab5ce4da7d642cad425a
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-06 10:42:25 +0100

    50A - Domino piling

---
+++ CodeForces/DominoPiling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace DominoPiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] input = Console.ReadLine().Split(' ').Select(n => Convert.ToByte(n)).ToArray();
+
+            if (input[0] % 2 == 0)
+            {
+                Console.WriteLine((input[0] / 2) * input[1]);
+            }
+            else if (input[1] % 2 == 0)
+            {
+                Console.WriteLine((input[1] / 2) * input[0]);
+            }
+            else if (input[0] > 2)
+            {
+                Console.WriteLine(((input[0] / 2) * input[1] + input[1] / 2));
+            }
+            else if (input[1] > 2)
+            {
+                Console.WriteLine(((input[1] / 2) * input[0] + input[0] / 2));
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
