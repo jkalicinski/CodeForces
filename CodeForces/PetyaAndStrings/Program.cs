be884760993b76e2d06681c9b1329ea95d19262b
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-06 13:00:55 +0100

    112A - Petya and Strings

---
+++ CodeForces/PetyaAndStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PetyaAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            int result = 0;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                int charValF = (sbyte)first[i];
+                int charValS = (sbyte)second[i];
+
+                if (charValF >= 97)
+                    charValF = charValF - 32;
+
+                if (charValS >= 97)
+                    charValS = charValS - 32;
+
+                if (charValF > charValS)
+                {
+                    result = 1;
+                    break;
+                }
+                else if (charValF < charValS)
+                {
+                    result = -1;
+                    break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
