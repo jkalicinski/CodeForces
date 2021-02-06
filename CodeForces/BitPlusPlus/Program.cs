22eab7669814b0fe214c170dd111fa78e7d668d8
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-06 11:01:37 +0100

    282A - Bit++

---
+++ CodeForces/BitPlusPlus/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BitPlusPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte lines = Convert.ToByte(Console.ReadLine());
+            int value = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string line = Console.ReadLine();
+
+                if (line[0] == '+' || line[2] == '+')
+                    value++;
+                else
+                    value--;
+            }
+
+            Console.WriteLine(value);
+        }
+    }
+}
