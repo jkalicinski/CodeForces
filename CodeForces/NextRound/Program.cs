aefb1cec277754b6fad3e5f876fd29a7acb1510a
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-06 08:52:20 +0100

    nextround

---
+++ CodeForces/NextRound/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace NextRound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nk = Console.ReadLine();
+            string scores = Console.ReadLine();
+
+            byte[] nkarray = nk.Split(' ').Select(n => Convert.ToByte(n)).ToArray();
+            byte[] scoresarr = scores.Split(' ').Select(n => Convert.ToByte(n)).ToArray();
+
+            int counter = 0;
+
+            for (int i = 0; i < nkarray[0]; i++)
+            {
+                if (scoresarr[i] > 0 && scoresarr[i] >= scoresarr[nkarray[1] - 1])
+                    counter++;
+                else
+                    break;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
