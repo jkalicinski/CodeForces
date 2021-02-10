9fff7d813b108fab0e3b5e8458fddadf779c5efe
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-10 15:45:27 +0100

    236A - Boy or Girl

---
+++ CodeForces/BoyOrGirl/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BoyOrGirl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = "CHAT WITH HER!";
+
+            if (DistinctCount(input) % 2 != 0)
+            {
+                result = "IGNORE HIM!";
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int DistinctCount(string input)
+        {
+            int distinct = 0;
+            char[] array = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] == input[i])
+                    {
+                        break;
+                    }
+
+                    if (array[j] == char.MinValue)
+                    {
+                        array[j] = input[i];
+                        distinct++;
+                        break;
+                    }    
+                }
+            }
+
+            return distinct;
+        }
+    }
+}
