b5dd1e98d1c996a8cedc722fed50332b52930e67
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-10 16:56:56 +0100

    791A - Bear and Big Brother

---
+++ CodeForces/BearAndBigBrother/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace BearAndBigBrother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            int years = 0;
+
+            while (input[0] <= input[1])
+            {
+                years++;
+                input[0] = input[0] * 3;
+                input[1] = input[1] * 2;
+            }
+
+            Console.WriteLine(years);
+        }
+    }
+}
