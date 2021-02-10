77b2399e1f4d4d132fc808a4ed1abc166df33dbe
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-10 16:19:12 +0100

    546A - Soldier and Bananas

---
+++ CodeForces/SoldierAndBananas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SoldierAndBananas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            int sum = 0;
+
+            for (int i = 1; i <= input[2]; i++)
+            {
+                sum += i * input[0]; 
+            }
+
+            if (sum - input[1] >= 0)
+            {
+                Console.WriteLine(sum - input[1]);
+            }
+            else 
+            {
+                Console.WriteLine(0);
+            }           
+        }
+    }
+}
