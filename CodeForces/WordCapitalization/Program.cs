63480856d89251d176d569d7b597640fc8eaa0a8
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-10 14:25:29 +0100

    281A - Word Capitalization

---
+++ CodeForces/WordCapitalization/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WordCapitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if ((byte)input[0] > 96)
+            {
+                Console.WriteLine($"{(char)((byte)input[0] - 32)}{input.Substring(1, input.Length - 1)}");
+            }
+            else
+            {
+                Console.WriteLine(input); 
+            }
+        }
+    }
+}
