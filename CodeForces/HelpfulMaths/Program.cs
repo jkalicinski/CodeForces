a591b2b55961a2336ae0a0e09fd28d5af80f7724
Jaroslaw Kalicinski <jak0024@arribatec.com>
2021-02-10 14:10:05 +0100

    tbf

---
+++ CodeForces/HelpfulMaths/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelpfulMaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] numbers;
+
+            if (input.Length > 1)
+            {
+                int arrayLength = input.Length - (((input.Length + 1) - (input.Length + 1) / 2) - 1);
+                numbers = new int[arrayLength];
+                int index = 0;
+
+                for (int i = 0; i < input.Length; i = i + 2)
+                {
+                    numbers[index] = Convert.ToInt32(input[i]);
+                    index++;
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine(input[0]);
+            }
+        }
+    }
+}
